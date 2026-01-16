03b3289490d11e36f2c0b5b8bd99e11bc4209ca3
Anthony Prates de Vargas <anthony.vargas.dev@outlook.com>
2026-01-16 16:28:05 -0300

    add: config project

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
-console.log("Hello word")
+import { app } from "./server";
+
+const start = async () => {
+    await app.listen({ port: 3333 });
+    console.log("Server running on http://localhost:3333"); 
+}
+
+start();
